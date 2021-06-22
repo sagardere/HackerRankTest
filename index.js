5a9233ce3ccca7f002d5374c134f3af7f2f24cf7
Dere Sagar <deresagar01@gmail.com>
2021-06-22 09:20:39 +0530

    Added some chnages

---
+++ index.js
@@ -0,0 +1,37 @@
+const projects = require('./data-store');
+const http = require('http');
+const async = require('async');
+let server;
+
+const requestListener = async function(req, res) {
+  var url = req.url;
+
+  if (url && url.indexOf('/projects/' !== -1) && req.method === 'GET') {
+    console.log(url.split('/'))
+    var badMsg = {
+      'message': 'BAD REQUEST'
+    };
+    if (url && url.split('/') && url.split('/')[2]) {
+      var id = parseInt(url.split('/')[2]);
+      var result = await projects.filter(project => project.id == id);
+    }
+    if(result && result.length > 0){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(result));
+    } else {
+      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.end('BAD REQUEST');
+    }
+  } else {
+    res.writeHead(400, {'Content-Type': 'application/json'});
+    res.end('BAD REQUEST');
+  }
+}
+
+server = http.createServer(requestListener);
+server.listen(8000, function() {
+  console.log("server start at port 8000");
+});
+
+
+module.exports = server;    
